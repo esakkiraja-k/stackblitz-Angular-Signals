288f55a4310c5184a66ba39c5817655ccef1cdd1
esakkiraja-k <esakkirajait@gmail.com>
2024-08-13 13:38:48 +0530

    Computed signals

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -18,6 +18,7 @@ import { FormsModule } from '@angular/forms';
   <div>{{ quantity() }} </div>
   <div>Product: {{selectedProduct().name}}</div>
   <div>Product: {{selectedProduct().price}}</div>
+  <div [style.color]='color()' >Extended Price: {{exPrice() | currency }}</div>
   `,
 })
 export class App {
@@ -26,10 +27,17 @@ export class App {
   qtyAvailable = signal([1, 2, 3, 4, 5]);
   selectedProduct = signal<Product>({ id: 5, name: 'pot', price: 12 });
 
+  exPrice = computed(() => this.selectedProduct().price * this.quantity());
+  color = computed(() => this.exPrice() > 50 ? 'green': 'blue');
   constructor() {
     console.log('Constructor', this.quantity());
+    effect(() => console.log('In effect',this.quantity()));
   }
-  onQunatitySelected(qty: number) {}
+  onQunatitySelected(qty: number) {
+    this.quantity.set(qty);
+    // this.quantity.set(22);
+    // this.quantity.set(100);
+  }
 }
 export interface Product {
   id: number;
